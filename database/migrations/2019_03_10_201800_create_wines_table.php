262aa586d3e73be37f3e185ccbddaf0f7ac8775d
Bruno Krebs <bruno.krebs@auth0.com>
2019-03-10 17:32:32 -0300

    migration script for the Wines model;

---
+++ database/migrations/2019_03_10_201800_create_wines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->text('description');
+            $table->string('color', 10);
+            $table->string('grape_variety', 50);
+            $table->string('country', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wines');
+    }
+}
